1b0c167426dd96f97f17eadb776c81662a668d1f
Vipul Pachauri <vipulpachauri12@gmail.com>
2026-04-13 08:40:47 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,36 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+        
+    }
+    
+    public void pop() {
+        Integer popped = stack.pop();
+        if(popped.equals(minStack.peek())){
+            minStack.pop();
+        }
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
